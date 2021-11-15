a069267d156965022230b8009204040c01b2f9a6
murasaki21 <47200816+murasaki21@users.noreply.github.com>
2021-11-15 15:32:56 -0800

    Update senbazuru.js

--- senbazuru.js
+++ senbazuru.js
@@ -186,14 +186,14 @@ function init() {
   scene.add(floor);
 
 
-  // Generate objects (cubes)
+  // Generate objects (cubes) *replace with cranes*
   const boxGeometry = new THREE.BoxGeometry(20, 20, 20).toNonIndexed();
 
   position = boxGeometry.attributes.position;
   const colorsBox = [];
 
   for (let i = 0, l = position.count; i < l; i++) {
-    color.setHSL(Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75);
+    color.setHSL(Math.random() * 0.3 + 0.8, 0.75, Math.random() * 0.25 + 0.75);
     colorsBox.push(color.r, color.g, color.b);
   }
 
