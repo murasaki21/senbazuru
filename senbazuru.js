00fd417c415a23ed45181da2bd7d0543fa7a4b99
murasaki21 <47200816+murasaki21@users.noreply.github.com>
2021-11-17 01:35:32 -0800

    Update senbazuru.js

--- senbazuru.js
+++ senbazuru.js
@@ -48,6 +48,7 @@ function init() {
   // Define basic scene parameters
   scene = new THREE.Scene();
   scene.background = new THREE.Color(0xFFd6FF);
+  //color of the world edge
   scene.fog = new THREE.Fog(0xffffff, 0, 750);
 
   // Define scene lighting
