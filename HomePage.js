0e5b9982c30c603924ce411ff08368e7a3f11851
murasaki21 <47200816+murasaki21@users.noreply.github.com>
2021-11-17 02:00:01 -0800

    Create HomePage.js

---
+++ HomePage.js
@@ -0,0 +1,121 @@
+import * as THREE from "./buildJS/three.module.js";
+
+// Import add-ons for glTF models, orbit controls, and font loader
+import {
+  OrbitControls
+} from "../sourceJS/OrbitControls.js";
+import {
+  GLTFLoader
+} from "../sourceJS/GLTFLoader.js";
+
+let container, scene, camera, renderer, mesh, mesh2, mixer, controls, clock;
+
+let ticker = 0;
+
+// Call init and animate functions (defined below)
+init();
+animate();
+
+function init() {
+
+  //Identify div in HTML to place scene
+  container = document.getElementById("space");
+
+  //Crate clock for animation
+  clock = new THREE.Clock();
+
+  //Create scene
+  scene = new THREE.Scene();
+  camera = new THREE.PerspectiveCamera(
+    50,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
+  renderer = new THREE.WebGLRenderer();
+  //changes space color
+  renderer.setClearColor(0x000000);
+  //changes canvas size
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth / 2, window.innerHeight / 2);
+  // Add scene to gltf.html
+  container.appendChild(renderer.domElement);
+
+  // Material to be added to preanimated model
+  var newMaterial = new THREE.MeshStandardMaterial({
+    color: 0x99B9FF
+  });
+
+  // Load preanimated model, add material, and add it to the scene
+  const loader = new GLTFLoader().load(
+    "../gltfs/AnimatedCrane.glb",
+    function(gltf) {
+      gltf.scene.traverse(function(child) {
+        if (child.isMesh) {
+          child.material = newMaterial;
+        }
+      });
+      // set position and scale
+      mesh = gltf.scene;
+      mesh.position.set(4, 0, 0);
+      mesh.rotation.set(0, 0, 0);
+      mesh.scale.set(2, 2, 2);
+      // Add model to scene
+      scene.add(mesh);
+      //Check for and play animation frames
+      mixer = new THREE.AnimationMixer(mesh);
+      gltf.animations.forEach((clip) => {
+        mixer.clipAction(clip).play();
+      });
+
+    },
+    undefined,
+    function(error) {
+      console.error(error);
+    }
+  );
+
+  // Add Orbit Controls
+  controls = new OrbitControls(camera, renderer.domElement);
+  controls.minDistance = 3;
+  controls.maxDistance = 25;
+  controls.target.set(0, 0, -0.2);
+
+  // Position our camera so we can see the shape
+  camera.position.x = 8;
+  camera.position.z = 13;
+
+  // Add a directional light to the scene
+  const directionalLight = new THREE.DirectionalLight(0xffffff, 0.9);
+  scene.add(directionalLight);
+
+  // Add an ambient light to the scene
+  const ambientLight = new THREE.AmbientLight(0xffffff, 0.9);
+  scene.add(ambientLight);
+}
+
+// Define animate loop
+function animate() {
+  controls.update();
+  requestAnimationFrame(animate);
+  var delta = clock.getDelta();
+  if (mixer) mixer.update(delta);
+  render();
+}
+
+// Define the render loop
+function render() {
+  renderer.render(scene, camera);
+
+}
+
+// Respond to Window Resizing
+window.addEventListener("resize", onWindowResize);
+
+// Window resizing function
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth / 2, window.innerHeight / 2);
+  render();
+}
