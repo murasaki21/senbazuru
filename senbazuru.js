37cbe65c607ce4324aaddeb4a9aa6a22702f77b8
murasaki21 <47200816+murasaki21@users.noreply.github.com>
2021-11-17 15:25:31 -0800

    change crane position

--- senbazuru.js
+++ senbazuru.js
@@ -243,7 +243,7 @@ const loader2 = new GLTFLoader().load(
     });
     // set position and scale
     mesh2 = gltf.scene;
-    mesh2.position.set(1, 0, 200);
+    mesh2.position.set(0, 0, 300);
     mesh2.rotation.set(0, 0, 0);
     mesh2.scale.set(3, 3, 3);
 
