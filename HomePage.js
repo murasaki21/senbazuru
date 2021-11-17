57b353b7b187b4dc913aada72eb6d85f7b330877
murasaki21 <47200816+murasaki21@users.noreply.github.com>
2021-11-17 02:50:30 -0800

    add senbazuru ext to crane glb

--- HomePage.js
+++ HomePage.js
@@ -48,7 +48,7 @@ function init() {
 
   // Load preanimated model, add material, and add it to the scene
   const loader = new GLTFLoader().load(
-    "../gltfs/AnimatedCrane.glb",
+    "../senbazuru/gltfs/AnimatedCrane.glb",
     function(gltf) {
       gltf.scene.traverse(function(child) {
         if (child.isMesh) {
